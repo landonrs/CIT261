d628a396a10c38371b3c642d933dc26ba27e9c18
landonrs <altruist8@yahoo.com>
2018-12-03 20:04:56 -0700

    added styling and error handling
    This commit includes card styling for the recipe results and color
    to the header and body. Also added error message if users query
    does not return results.

--- recipe_finder/js/recipeSearcher.js
+++ recipe_finder/js/recipeSearcher.js
@@ -17,40 +17,46 @@ class RecipeSearcher {
         let recipeNum = recipes.count;
         let results = recipes.recipes;
 
-        for (var i = 0; i < recipeNum; i += 3) {
-            if(recipeNum - i < 3) {
-                break;
+        if (recipeNum >= 3) {
+
+            for (var i = 0; i < recipeNum; i += 3) {
+                if(recipeNum - i < 3) {
+                    break;
+                }
+                // fill in section with 3 recipes per row
+                let rSection = document.createElement("section");
+                rSection.setAttribute("class", "resultRow");
+                let recipeDivs = `
+                <div class="leftImg foodBox">
+                    <figure>
+                        <figcaption class="foodTitle">${results[i].title}</figcaption>
+                        <a href="${results[i].source_url}" target="_blank">
+                            <img class="foodImg" src="${results[i].image_url}">
+                        </a>
+                    </figure>
+                </div>
+                <div class="centerImg foodBox">
+                    <figure>
+                        <figcaption class="foodTitle">${results[i + 1].title}</figcaption>
+                        <a href="${results[i + 1].source_url}" target="_blank">
+                            <img class="foodImg" src="${results[i + 1].image_url}">
+                        </a>
+                    </figure>
+                </div>
+                <div class="rightImg foodBox">
+                    <figure>
+                        <figcaption class="foodTitle">${results[i + 2].title}</figcaption>
+                        <a href="${results[i + 2].source_url}" target="_blank">
+                            <img class="foodImg" src="${results[i + 2].image_url}">
+                        </a>
+                    </figure>
+                </div>`;
+                rSection.innerHTML = recipeDivs;
+                resultsDiv.appendChild(rSection);
             }
-            // fill in section with 3 recipes per row
-            let rSection = document.createElement("section");
-            rSection.setAttribute("class", "resultRow");
-            let recipeDivs = `
-            <div class="leftImg foodBox">
-                <figure>
-                    <figcaption class="foodTitle">${results[i].title}</figcaption>
-                    <a href="${results[i].source_url}" target="_blank">
-                        <img class="foodImg" src="${results[i].image_url}">
-                    </a>
-                </figure>
-            </div>
-            <div class="centerImg foodBox">
-                <figure>
-                    <figcaption class="foodTitle">${results[i + 1].title}</figcaption>
-                    <a href="${results[i + 1].source_url}" target="_blank">
-                        <img class="foodImg" src="${results[i + 1].image_url}">
-                    </a>
-                </figure>
-            </div>
-            <div class="rightImg foodBox">
-                <figure>
-                    <figcaption class="foodTitle">${results[i + 2].title}</figcaption>
-                    <a href="${results[i + 2].source_url}" target="_blank">
-                        <img class="foodImg" src="${results[i + 2].image_url}">
-                    </a>
-                </figure>
-            </div>`;
-            rSection.innerHTML = recipeDivs;
-            resultsDiv.appendChild(rSection);
+        }
+        else {
+            resultsDiv.innerHTML = '<h2 id="searchError">Your search did not return enough results!</h2>';
         }
     };
 }
