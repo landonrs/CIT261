8e0b9ec208d0e408c227c31845aa153801330afe
landonrs <altruist8@yahoo.com>
2018-12-07 14:36:14 -0700

    Added unit tests and npm script
    This commit adds more robust unit tests for checking the
    recipe result formatting. Also added simple bash script
    for experimenting how to run scripts using npm.

---
+++ recipe_finder/gitScript.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# filename: gitScript.sh
+cd..
+git add -A
+git commit
+git push
--- recipe_finder/test/recipeSearcherTest.js
+++ recipe_finder/test/recipeSearcherTest.js
@@ -4,35 +4,48 @@ var recipeSearcher = require('../js/recipeSearcher');
 var jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-
 const domString = '<!DOCTYPE html><html><head></head><body><button id="searchRatingButton" type="button"><button id="searchTrendyButton" type="button">search recent popular recipes</button>search recipes</button><div id="searchResults"></div></body></html>';
 const dom = new JSDOM(domString);
+// use this to verify the function generates the correct html
+var recipeResults = {
+    count: 6,
+    recipes:[
+        {"title": "pizza", "image_url":"pizza.png", "source_url": "#"}, 
+        {"title": "pizza 2", "image_url":"pizza.png", "source_url": "#"},
+        {"title": "pizza 3", "image_url":"pizza.png", "source_url": "#"},
+        {"title": "pizza 4", "image_url":"pizza.png", "source_url": "#"},
+        {"title": "pizza 5", "image_url":"pizza.png", "source_url": "#"},
+        {"title": "pizza 6", "image_url":"pizza.png", "source_url": "#"}
+    ]        
+}
 
 global.window = dom.window;
 global.document = dom.window.document;
 
 describe("populating result list", function() {
-    it("should add section to results", function(){
+    it("should add sections to results", function(){
         let resultDiv = document.getElementById("searchResults");
-        let recipeResults = {
-            count: 6,
-            recipes:[
-                {"title": "pizza", "image_url":"pizza.png", "source_url": "#"}, 
-                {"title": "pizza 2", "image_url":"pizza.png", "source_url": "#"},
-                {"title": "pizza 3", "image_url":"pizza.png", "source_url": "#"},
-                {"title": "pizza 4", "image_url":"pizza.png", "source_url": "#"},
-                {"title": "pizza 5", "image_url":"pizza.png", "source_url": "#"},
-                {"title": "pizza 6", "image_url":"pizza.png", "source_url": "#"}
-            ]        
-        }
-      
+
+        // test
         recipeSearcher.formResultPage(recipeResults);
+        
         // verify
-        console.log(resultDiv.innerHTML);
         assert.isTrue(resultDiv.hasChildNodes());
+        let rows = resultDiv.getElementsByClassName("resultRow");
+        assert.equal(rows.length, 2);
     });
 
-    it("should search by rating", function() {
+    it("should add images with links", function(){
+        let resultDiv = document.getElementById("searchResults");
+        // test
+        recipeSearcher.formResultPage(recipeResults);
+        
+        // verify that images have link parent with target attribute
+        let images = resultDiv.getElementsByClassName("foodImg");
+        assert.isTrue(images[0].parentNode.hasAttribute("target"));
+    });
+
+    it("should generate correct URL", function() {
         API_KEY = "12345";
 
         //test
@@ -40,7 +53,5 @@ describe("populating result list", function() {
         
         // verify
         assert.equal("https://www.food2fork.com/api/search?key=12345&q=chicken&sort=r", recipeSearcher.getURL());
-
     }); 
-
 });
