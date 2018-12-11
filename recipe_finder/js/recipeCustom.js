a217a35acd68b200e03c7aa182e6b92044a6d34e
landonrs <altruist8@yahoo.com>
2018-12-10 19:03:51 -0700

    added swiping customize feature
    This commit adds a new js file which handles swiping events. The user can
    now change the color between a french flag scheme and an italian flag
    look. This feature was mainly added so I could experiment with handling
    swiping motion in vanilla js.

---
+++ recipe_finder/js/recipeCustom.js
@@ -0,0 +1,36 @@
+var touchPoint = null;
+
+window.addEventListener("touchstart", function(event) {
+    if(event.touches.length === 1) {
+        touchPoint = event.touches.item(0).clientX;
+    }
+    else {
+        touchPoint = null;
+    }
+})
+
+window.addEventListener("touchend", function(event) {
+    var offset = 300;
+    let header = document.getElementById("header");
+    let results = document.getElementById("searchResults");
+
+    if(touchPoint != null) { 
+        var liftPoint = event.changedTouches.item(0).clientX;
+
+        // left to right swipe
+        if (liftPoint > touchPoint + offset) {
+            console.log("swiped Right!");
+            header.setAttribute("style", "background-color:blue;");
+            results.setAttribute("style", "background-color:red;");
+            //this.document.body.style.backgroundImage = "none";
+
+        }
+        // right to left
+        else if (liftPoint < touchPoint - offset) {
+            console.log("swiped Left!");
+            header.setAttribute("style", "background-color:rgb(218, 15, 0);");
+            results.setAttribute("style", "background-color:rgba(0, 61, 10, 0.877);");
+            //this.document.body.style.backgroundImage = "url('yellow-bg.jpg')";
+        }
+    }
+})
